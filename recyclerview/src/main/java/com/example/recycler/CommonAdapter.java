e19c5313bd651752f5ce27c63448629ad2271a33
XiaoGui <1057415595@qq.com>
2018-12-20 22:00:30 +0800

    Initial commit

---
+++ recyclerview/src/main/java/com/example/recycler/CommonAdapter.java
@@ -0,0 +1,125 @@
+package com.example.recycler;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class CommonAdapter<T> extends RecyclerView.Adapter<CommonViewHolder> {
+
+    protected Context mContext;
+    protected int mLayoutId;
+    protected List<T> mDatas;
+    protected LayoutInflater mInflater;
+    protected RecyclerView recyclerView;
+
+    private CommonOnItemClickListener mOnItemClickListener;
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        this.recyclerView = recyclerView;
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView);
+        this.recyclerView = null;
+    }
+
+    public void setOnItemClickListener(CommonOnItemClickListener onItemClickListener)
+    {
+        this.mOnItemClickListener = onItemClickListener;
+    }
+
+    public CommonAdapter(Context context, int layoutId){
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mLayoutId = layoutId;
+        mDatas = new ArrayList<>();
+    }
+
+    public CommonAdapter(Context context, int layoutId, List<T> datas)
+    {
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mLayoutId = layoutId;
+        mDatas = datas;
+    }
+
+    @Override
+    public CommonViewHolder onCreateViewHolder(final ViewGroup parent, int viewType)
+    {
+        CommonViewHolder viewHolder = CommonViewHolder.get(mContext, null, parent, mLayoutId, -1);
+        setListener(parent, viewHolder, viewType);
+        return viewHolder;
+    }
+
+    protected int getPosition(RecyclerView.ViewHolder viewHolder)
+    {
+        return viewHolder.getAdapterPosition();
+    }
+
+    protected boolean isEnabled(int viewType)
+    {
+        return true;
+    }
+
+
+    protected void setListener(final ViewGroup parent, final CommonViewHolder viewHolder, int viewType)
+    {
+        if (!isEnabled(viewType)) return;
+        viewHolder.getConvertView().setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (mOnItemClickListener != null)
+                {
+                    int position = getPosition(viewHolder);
+                    mOnItemClickListener.onItemClick(parent, v, mDatas.get(position), position);
+                }
+            }
+        });
+
+
+        viewHolder.getConvertView().setOnLongClickListener(new View.OnLongClickListener()
+        {
+            @Override
+            public boolean onLongClick(View v)
+            {
+                if (mOnItemClickListener != null)
+                {
+                    int position = getPosition(viewHolder);
+                    return mOnItemClickListener.onItemLongClick(parent, v, mDatas.get(position), position);
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onBindViewHolder(CommonViewHolder holder, int position)
+    {
+        holder.updatePosition(position);
+        convert(holder, mDatas.get(position));
+    }
+
+    public abstract void convert(CommonViewHolder holder, T t);
+
+    public void setmDatas(List<T> list){
+        mDatas = list;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return mDatas.size();
+    }
+}
